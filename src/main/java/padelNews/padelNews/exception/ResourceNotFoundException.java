fab4230aa172c0b734e11515e48bd54b9281e5c4
mise-a-jour-backend <bobypro225@gmail.com>
2026-04-20 15:41:03 +0200

    mise en place exception et datainitializer

---
+++ src/main/java/padelNews/padelNews/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package padelNews.padelNews.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
